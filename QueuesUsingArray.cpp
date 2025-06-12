69627aba148c5b74beee93d058a2a03b3b05057e
Yuratama1 <yuratama661@gmail.com>
2025-06-12 15:32:10 +0700

    memebuat program cek apakah antrian kososng dan jika rear di akhir array akan kembali ke awal

--- QueuesUsingArray.cpp
+++ QueuesUsingArray.cpp
@@ -22,3 +22,19 @@ class Queue{
                 cout << "\nQueue overlow\n";//1.a
                 return; //1.b
             }
+            //2. cek apakah antrian kosong
+            if (FRONT == -1){
+                FRONT = 0; //2.a
+                REAR = 0; //2.b
+            }
+            else{
+                //jika rear brada di posisi terakhir array, kembali ke awal array
+                if(REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
+        }
+
+        
