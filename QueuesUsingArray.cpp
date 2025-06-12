2a43f2a114c7d3dd62c29f1f2350cef7dd836436
Yuratama1 <yuratama661@gmail.com>
2025-06-12 15:30:06 +0700

    membuat program mengecek apakah antrian penuh

--- QueuesUsingArray.cpp
+++ QueuesUsingArray.cpp
@@ -16,5 +16,9 @@ class Queue{
             cout << "Enter a number : ";
             cin >> num;
             cout << endl;
-        }
-};
+
+            //1. cek apakah antrian penuh
+            if((FRONT == 0 && REAR == max - 1) || (FRONT = REAR + 1)){
+                cout << "\nQueue overlow\n";//1.a
+                return; //1.b
+            }
