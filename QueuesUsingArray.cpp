42af5457b6d1cb81f22fc1d3e72f4642b6afd553
Yuratama1 <yuratama661@gmail.com>
2025-06-12 15:39:02 +0700

    membuat program utama dengan membuat class objek serta mengunakan perulangan while untuk membuat pilihan case

--- QueuesUsingArray.cpp
+++ QueuesUsingArray.cpp
@@ -93,3 +93,44 @@ class Queue{
         }
 };
 
+int main(){
+    Queue q;
+    char ch;
+
+    while(true){
+        try{
+            cout << "Menu" << endl;
+            cout << "1. Implement insert opration" << endl;
+            cout << "2. Implement delete opration" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice" << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch(ch){
+                case '1':{
+                    q.insert();
+                    break;
+                }
+                case '2':{
+                    q.remove();
+                    break;
+                }
+                case '3':{
+                    q.display();
+                }
+                case '4':{
+                    return 0;
+                }
+                default:{
+                    cout << "Invalid option" << endl;
+                    break;
+                }
+            }
+        }
+        catch (exception &e){
+            cout << "Check for the values entered." << endl;
+        }
+    }
+}
